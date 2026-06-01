cd32d56c4e24d22922b32c0ecb3ec7f94f22e604
khushboo neema <khushboo.neema@gmail.com>
2026-06-01 16:15:20 -0400

    Initial Commit

---
+++ PostureKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PostureKit
+//
+//  Created by Khushboo Neema on 6/1/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
